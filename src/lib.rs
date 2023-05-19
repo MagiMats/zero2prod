d5694bbf39fb94fa2d1966129dda3b459880c7fd
Mats <matsjanssens@gmail.com>
2023-05-19 19:05:59 +0200

    Stage

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 //! src/lib.rs
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
